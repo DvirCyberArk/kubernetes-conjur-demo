0005b23e1b80e6dc72560781e42bdb6b69f06e5f
Geri Jennings <geri.jennings@cyberark.com>
2018-08-14 15:54:39 -0400

    Update the demo to use cyberark/demo-app
    - Remove old test apps
    - Build/deploy a PostgreSQL instance when apps are built/deployed
    - Store DB username and URL in Conjur policy as well as password
    - Update naming to use "summon" instead of "api"
    - Update validation test to POST and GET to the API of the demo apps
    - Expose apps over port 8080
    - Update app manifests to enable them to work with Summon

--- 5_build_and_push_containers.sh
+++ 5_build_and_push_containers.sh
@@ -7,14 +7,26 @@ if [ $PLATFORM = 'openshift' ]; then
     docker login -u _ -p $(oc whoami -t) $DOCKER_REGISTRY_PATH
 fi
 
-announce "Building and pushing test app image."
+announce "Building and pushing test app images."
 
-pushd $TEST_APP_NAME
-  ./build.sh
+pushd test_app
+  docker build -t test-app:$CONJUR_NAMESPACE_NAME .
+
+  test_app_image=$(platform_image test-app)
+  docker tag test-app:$CONJUR_NAMESPACE_NAME $test_app_image
+
+  if [[ $MINIKUBE != true ]]; then
+    docker push $test_app_image
+  fi
 popd
 
-test_app_image=$(platform_image $TEST_APP_NAME)
-docker tag $TEST_APP_NAME:$TEST_APP_NAMESPACE_NAME $test_app_image
-if [[ $MINIKUBE != true ]]; then
-  docker push $test_app_image
-fi
+pushd pg
+  docker build -t test-app-pg:$CONJUR_NAMESPACE_NAME .
+
+  test_app_pg_image=$(platform_image test-app-pg)
+  docker tag test-app-pg:$CONJUR_NAMESPACE_NAME $test_app_pg_image
+
+  if [[ $MINIKUBE != true ]]; then
+    docker push $test_app_pg_image
+  fi
+popd
--- webapp/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -e
-docker build -t webapp:$TEST_APP_NAMESPACE_NAME .
